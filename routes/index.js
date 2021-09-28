d68fc269af59d94606b0e448c294e1108a6bb746
Zhaoji Chen <kb1002279894@gmail.com>
2021-09-28 01:04:11 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home'});
+});
+
+/* GET about us page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About'});
+});
+
+/* GET products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products'});
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services'});
+});
+
+/* GET contact us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact'});
+});
+
+module.exports = router;
