2bfb67bd6d7189be7c885dca730861f33cb4266d
Zhaoji Chen <kb1002279894@gmail.com>
2021-10-26 15:27:09 -0400

    assignment2

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/contact"
+    // "URI": "mongodb://localhost/contact"
+    "URI": "mongodb+srv://ZhaojiChen:James1002279894@czjmongodbserver.kzmfp.mongodb.net/contact?retryWrites=true&w=majority"
 }
