65ff6ddaeed630f35c7c1d28eb37d82c579460fa
Zhaoji Chen <kb1002279894@gmail.com>
2021-10-26 16:31:35 -0400

    Assigment2

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = 
 {
     // "URI": "mongodb://localhost/contact"
-    "URI": "mongodb+srv://ZhaojiChen:James1002279894@czjmongodbserver.kzmfp.mongodb.net/contact?retryWrites=true&w=majority"
+    "URI": "mongodb+srv://ZhaojiChen:James1002279894@czjmongodbserver.kzmfp.mongodb.net/contact?retryWrites=true&w=majority",
+    "Secret":'SomeSecret'
 }
